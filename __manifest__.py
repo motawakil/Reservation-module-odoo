dffaa99d586daa01bb777e1a916e214b7339d3ad
motaouakel <motaouakelmaimo19@gmail.com>
2026-02-16 16:23:32 +0000

    reservation: add the module structure

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+"name" : "Reservation",
+"version" : "1.0",          
+"depends" : ['base', 'mail','sale', 'product','portal','website'],
+"author" : "Motaouakel",
+"summary" : "Manage reservations",
+"description" : "A module to manage reservations.",         
+"category" : "Uncategorized",       
+"application" : True,
+"instalable" : True,
+}
+
